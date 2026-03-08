c21408a67b96a7c594a0215d6afb8b7236d8d712
Andreea Nedelcu <andreea.nedelcu1204@gmail.com>
2026-03-08 23:17:14 +0200

    first commit: pokemon slot test

---
+++ my-app/src/Components/PokemonSlot/PokemonSlot.tsx
@@ -0,0 +1,11 @@
+import './PokemonSlot.css'
+
+const PokemonSlot = () => {
+  return (
+    <div>
+        <div className='circle'><img src="https://play.pokemonshowdown.com/sprites/home-centered/aromatisse.png" /></div>
+    </div>
+  )
+}
+
+export default PokemonSlot
---
+++ my-app/src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import PokemonSlot from './Components/PokemonSlot/PokemonSlot.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <PokemonSlot />
+  </StrictMode>,
+)
